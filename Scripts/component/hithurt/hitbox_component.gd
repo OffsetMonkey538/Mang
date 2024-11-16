09dc6d3ea777d692ca8b4ca7180e397d39b658fb
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-11-16 22:35:14 +0200

    Vastaste elude vähendamine
    Nüüd kui vastastel on olemas elud, on vaja teha mängijale võimalikuks nende alandamine. Selleks on vaja lausa kahte uut komponenti! Nendeks on HitboxComponent ja HurtboxComponent.
    HitboxComponent tähistab ala, mis kokkupuutel HurtboxComponent-iga vähendab oma omaniku elusid.
    Hetkel on mängijal HurtboxComponent ja vastastel HitboxComponent, seega kui vastane puudutab mängijat, saab vastane haiget. Kui vastase elud jõuavad nullini, kaob ta ära.

---
+++ Scripts/component/hithurt/hitbox_component.gd
@@ -0,0 +1,13 @@
+class_name HitboxComponent extends Area2D
+
+@export var health_component: HealthComponent;
+
+signal hit(by: HurtboxComponent)
+
+func hurtbox_entered(hurtbox: HurtboxComponent) -> void:
+	if hurtbox.is_queued_for_deletion(): return;
+	if self.is_queued_for_deletion(): return;
+	
+	hit.emit(hurtbox);
+	
+	if (health_component): health_component.remove_health(hurtbox.damage);
---
+++ Scripts/component/hithurt/hurtbox_component.gd
@@ -0,0 +1,15 @@
+class_name HurtboxComponent extends Area2D
+
+@export var damage: int = 1;
+
+signal hit(what: HitboxComponent);
+
+func _ready() -> void:
+	area_entered.connect(hitbox_entered);
+
+func hitbox_entered(area: Area2D) -> void:
+	if not area is HitboxComponent: return;
+	if is_queued_for_deletion(): return;
+	
+	area.hurtbox_entered(self);
+	hit.emit(area);
