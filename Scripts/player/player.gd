eeb60a1dd6cc4f2a1e0d66d85e3a0100aa9adba9
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-11-16 17:24:09 +0200

    Mängija vaatamine telefonil
    Ringi vaatamiseks oleks vaja paremale poole teist joysticki, selleks saame lihtsalt esimest copy-paste'ida ning liigutada paremale. Korras? Ei. Hetkel kasutavad mõlemad joystick-id samu action-eid, "liigu_vasakule", "liigu_paremale", jne. Kuid parempoolse joystick-i jaoks peaks tegema uued action-id, "vaata_vasakule", "vaata_paremale", jne. Need tuleb teha sama moodi nagu enne (viide pildile/pigem vist videole mis näitab kuidas uut action-it lisada)
    
    Nüüd muudab küll joystick õigeid action-eid, kuid mäng ei kasuta neid kusagil, seega peab mängija skripti jälle muutma. (pilt lisatud koodist player.gd line 20 kuni 22)
    
    Kõigepealt küsib kood Godot-lt, millised vaatamis*action*-id on hetkel kasutusel, ning seejärel kasutab nende nurka mängija *rotation*-ina, kui aga vaatamissuund on null, ei muuda see *rotation*-it, seega võib mängija *joystick*-ist lahti lasta, ja ikka samasse suunda vaadata.
    
    Nüüd kui mäng telefoni laadida, on võimalik nii ringi liikuda kui ka vaatamissunda muuta

--- Scripts/player/player.gd
+++ Scripts/player/player.gd
@@ -9,10 +9,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var suund: Vector2 = Input.get_vector("liigu_vasakule", "liigu_paremale", "liigu_üles", "liigu_alla")
+	var liikumissuund: Vector2 = Input.get_vector("liigu_vasakule", "liigu_paremale", "liigu_üles", "liigu_alla")
 	
-	position += suund.normalized() * max_speed * delta;
+	position += liikumissuund.normalized() * max_speed * delta;
 	
 	
 	if (OS.has_feature("pc") and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
 		rotation = global_position.angle_to_point(get_global_mouse_position())
+	
+	var vaatamissuund: Vector2 = Input.get_vector("vaata_vasakule", "vaata_paremale", "vaata_üles", "vaata_alla");
+	if (vaatamissuund != Vector2.ZERO):
+		rotation = vaatamissuund.angle();
